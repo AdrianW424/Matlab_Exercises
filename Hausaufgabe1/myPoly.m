903eb4f03a893d154680f6b67395dc47dfb5752b
AdrianW424 <80892702+AdrianW424@users.noreply.github.com>
2022-04-05 17:46:10 +0200

    Add files via upload

---
+++ Hausaufgabe1/myPoly.m
@@ -0,0 +1,27 @@
+function y = myPoly(x)
+% myPoly - This function is used to represent a mathematical function
+%
+% Syntax:  y = myPoly(x)
+%
+% Inputs:
+%    x - x-argument of the function
+%
+% Outputs:
+%    y - the function value corresponding to the x-argument according to the mathematical formula
+%
+% Other m-files required: none
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also: dmyPoly,  myNewton
+%
+% MATLAB Version: MATLAB R2022a
+%
+% Author: Adrian Waldera
+% DHBW Stuttgart
+% email: adrianwaldera@gmx.de
+% April 2022; Last revision: 05-April-2022
+%------------------------------------------------------------
+
+    y = (x-3)^2;
+end
---
+++ Hausaufgabe1/numDiff.m
@@ -0,0 +1,40 @@
+function derivative = numDiff(func, x, Method)
+% numDiff - Calculates numerical derivative of a function
+%
+% Syntax:  derivative = numDiff(func, x, Method)
+%
+% Inputs:
+%    func - Function whose derivative is to be calculated
+%    x - Point at which the discharge is to take place
+%    Method - Method with which the derivation is to take place
+%
+% Outputs:
+%    derivative - Derived function at position x
+%
+% Other m-files required: none
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also: myPoly
+%
+% MATLAB Version: MATLAB R2022a
+%
+% Author: Adrian Waldera
+% DHBW Stuttgart
+% email: adrianwaldera@gmx.de
+% April 2022; Last revision: 05-April-2022
+%------------------------------------------------------------
+
+    if Method == "Vorwärtsdifferentiation"
+        h = 10^(-8);
+        derivative = (func(x + h) - func(x))/h;
+    elseif Method == "Rückwärtsdifferentiation"
+        h = 10^(-8);
+        derivative = (func(x) - func(x - h))/h;
+    elseif Method == "Zentraldifferentiation"
+        h = 10^(-6);
+        derivative = (func(x + h) - func(x - h))/(2*h);
+    else
+        error("Es wurde keine passende Methode zur numerischen Differentiation ausgewählt")
+    end
+end
