d5f67430dd09482452286a96b14794f57232789c
AdrianW424 <80892702+AdrianW424@users.noreply.github.com>
2022-04-05 17:43:11 +0200

    Delete Hausaufgabe1 directory

--- Hausaufgabe1/runMyNewton.m
+++
@@ -1,5 +0,0 @@
-[xZero, abortFlag, iters] = myNewton('function', @myPoly, 'livePlot', 'on');
-
-disp(['Zero at: x0 = ',num2str(xZero)]);
-disp(['Reason for the calculation abort: ', abortFlag]);
-disp(['Number of iterations: ',num2str(iters)]);
